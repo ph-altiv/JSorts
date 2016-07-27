87506dc13a192e901e1459edffbc5f9f74c4f4e6
Timofey Aleynikov <aleynikov-timofey@yandex.ru>
2016-07-27 15:58:43 +0300

    Controls

---
+++ scripts/actions.js
@@ -0,0 +1,5 @@
+function bRandom_Click()
+{
+	var t = $('input#tArrLen').val();
+	$('div#dResults').append(t + '<br/>');
+}
--- scripts/sorts.js
+++ scripts/sorts.js
@@ -8,8 +8,7 @@ console.log(s);
 var a = 10000000000;
 for(i = 1; i < 10000; i++)
 {
-	//document.write("SomeText<br/>");
-	var a = a/3;
+	a = a/3;
 }
 r = getMsec() - s;
 document.write(r + ' msec');
